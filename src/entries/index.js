fd80816f3cd8671cb7fcb0dde4596650bb69cfa6
Sanborn <io_sys@126.com>
2016-08-21 23:31:42 +0800

    project init

---
+++ src/entries/index.js
@@ -0,0 +1,71 @@
+import './index.html';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RedBox from 'redbox-react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer as routing } from 'react-router-redux';
+import reducers from '../reducers/index';
+import SagaManager from '../sagas/SagaManager';
+import Routes from '../routes';
+import './index.less';
+
+//////////////////////
+// Store
+
+const sagaMiddleware = createSagaMiddleware();
+const initialState = {};
+const enhancer = compose(
+  applyMiddleware(sagaMiddleware),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+const store = createStore(combineReducers({
+  ...reducers, routing,
+}), initialState, enhancer);
+SagaManager.startSagas(sagaMiddleware);
+
+if (module.hot) {
+  module.hot.accept('../reducers', () => {
+    const combinedReducers = combineReducers({ ...reducers, routing });
+    store.replaceReducer(combinedReducers);
+  });
+  module.hot.accept('../sagas/SagaManager', () => {
+    SagaManager.cancelSagas(store);
+    SagaManager.default.startSagas(sagaMiddleware);
+  });
+}
+
+//////////////////////
+// Render
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+let render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Routes history={history} />
+    </Provider>
+  , document.getElementById('root'));
+};
+
+if (module.hot) {
+  const renderNormally = render;
+  const renderException = (error) => {
+    ReactDOM.render(<RedBox error={error} />, document.getElementById('root'));
+  };
+  render = () => {
+    try {
+      renderNormally();
+    } catch (error) {
+      //console.error('error', error);
+      renderException(error);
+    }
+  };
+  module.hot.accept('../routes/index', () => {
+    render();
+  });
+}
+
+render();
